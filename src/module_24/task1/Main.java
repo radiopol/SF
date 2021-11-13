eb589a01f69040c20eab46540aea1a7a87ece900
Radiopol <qmallradiopol@gmail.com>
2021-11-13 15:22:15 +1000

    module_24.task1

---
+++ src/module_24/task1/Main.java
@@ -0,0 +1,26 @@
+package module_24.task1;
+
+public class Main {
+
+    public static void main(String[] args) {
+/*
+Make Alphabet class,
+so that this code and *similar ones* work correctly
+*/
+        Alphabet alphabet = new Alphabet();
+        alphabet.setW(5);
+        System.out.println(alphabet.getSum());
+        alphabet.setA(5);
+        alphabet.setS(7);
+        alphabet.setD(7);
+        System.out.println(alphabet.getSum());
+
+/*
+This fragment of code has to output
+
+5
+24
+
+*/
+    }
+}
